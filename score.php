797b0128058be2fb321ee5de0ca3ae49391a58bb
Hibikine Kage <goodsmile314159@gmail.com>
2017-12-23 04:04:26 +0900

    サーバーサイド実装

--- score.php
+++ score.php
@@ -4,11 +4,22 @@ $lines = file($filename);
 $score = $_POST['score'];
 for($i = 0; $i < count($lines); ++$i) {
   if ((int)$lines[$i] < $score) {
-    array_splice($lines, $i, 0, $score);
+    array_splice($lines, $i, 0, $score . "\n");
+    var_dump($lines);
     break;
   }
 }
-if(count($lines) === 0) {
-  $lines = [$score];
+if(count($lines) === $i) {
+  $lines = array_merge($lines, [$score . "\n"]);
 }
 file_put_contents($filename, $lines);
+include('index.html');
+?>
+
+
+<script>
+  const ranking = <?=json_encode(array_slice($lines, 5))?>;
+  const lastScore = <?=$score?>;
+  const rank = <?=$i?>;
+  showRanking(ranking, lastScore, rank);
+</script>
