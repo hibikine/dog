d31a6c246dffbbfacd0287d2a0db5d4aff8c559b
Mushus <mushus.wynd@gmail.com>
2017-12-30 23:30:40 +0900

    グラフィカル⊂ﾟＵ┬───┬~

--- src/index.js
+++ src/index.js
@@ -4,15 +4,39 @@ let count = 0;
 let lastNameIndex = 0;
 const $dog = $('#dog');
 const $countNum = $('#count-num');
+const dogStatus = {
+  defaultLength: 10, // 元の長さ
+  length: 10, // 現在の長さ
+  acceleration: 0, // 尻の加速度
+  unit: 25, // px/カウント
+  weight: 5, // 重さ(単位適当)
+  friction: 0.9, // 摩擦係数
+};
+
 
 function generateTwitterHref(n) {
   $('#twitter').attr('href', `https://twitter.com/intent/tweet?text=${encodeURIComponent(`すっぱいぬ🐶を${n}回伸ばしました！ https://hibikine.me/dog/ #のびるすっぱいぬ`)}`);
 }
 
+function drawDog() {
+  // 目指す長さ
+  const targetDogLength = dogStatus.defaultLength + count * dogStatus.unit;
+  // バネの強さ
+  const springForce = targetDogLength - dogStatus.length;
+  // 加速度を計算
+  dogStatus.acceleration += springForce / dogStatus.weight;
+  // 摩擦
+  dogStatus.acceleration *= dogStatus.friction;
+
+  // 長さを図る
+  dogStatus.length += dogStatus.acceleration;
+  $dog.css('width', `${dogStatus.length}px`);
+  window.requestAnimationFrame(drawDog);
+}
+
 // eslint-disable-next-line no-unused-vars
 $('#extend').click(() => {
   count += 1;
-  $dog.html(`${$dog.html()}─`);
   $countNum.html(count);
   generateTwitterHref(count);
   const meter = 1 + count / 2;
@@ -37,8 +61,11 @@ window.showRanking = (ranking, lastScore, rank) => {
   const yourRank = $(`<p>あなたのランク: ${rank + 1}位</p>`);
 
   const rankingWrapper = $('#ranking');
+  rankingWrapper.empty();
   rankingWrapper.append(rankingHeader);
   rankingWrapper.append(rankingList);
   rankingWrapper.append(yourRank);
 };
+
 generateTwitterHref(0);
+drawDog();
